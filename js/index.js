451b4f84ffb5992b4f485d503a195d43f78490a6
kennedy kiio <kiiokennedy38@gmail.com>
2022-03-23 17:00:22 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,74 @@
+//Scrollpy
+$(() => {
+    var navHeight = $(".navbar").outerHeight();
+    $('body').scrollspy({ target: ".navbar", offset: -100 });
+});
+
+//Arrays
+$("#array-form").submit((e) => {
+    e.preventDefault();
+    var array_string = "";
+    var arrays = $("[name='array']").val();
+    var capitalise = $("[name='capitalise']").val();
+    var array_items = arrays.split(",");
+    console.log(array_items.length);
+    var i = 1;
+
+    array_items.forEach((item) => {
+        if (i == 1) {
+            array_string += '<string-array name="name">\n    <item>' + handleCase(item, capitalise) + "</item>\n";
+        }
+        else if (i == array_items.length) {
+            array_string += "    <item>" + handleCase(item, capitalise) + "</item>\n<string-array>";
+        }
+        else {
+            array_string += "    <item>" + handleCase(item, capitalise) + "</item>\n";
+        }
+        i++;
+    });
+
+
+    //Result
+    $("#generated-array").val(array_string);
+});
+
+function handleCase(string, s_case) {
+    string = string.trim();
+    if (s_case == "lowercase") {
+        return string.toLowerCase();
+    }
+    else if (s_case == "capitalise-first") {
+        return string.charAt(0).toUpperCase() + string.substr(1).toLowerCase();
+    }
+    else {
+        return string.toUpperCase();
+    }
+}
+
+//generating menu
+$("#menu-form").submit((e) => {
+    e.preventDefault();
+    var menu_string_xml = "";
+    var menu_string_java = "";
+    var arrays = $("[name='menu-items']").val();
+    var capitalise = $("[name='capitalise-m']").val();
+    var array_items = arrays.split(",");
+    var i = 1;
+
+    array_items.forEach((item) => {
+        menu_string_xml +=
+            `<item
+                android:id="@+id/${handleCase(item, capitalise)}"
+                android:title="${handleCase(item, capitalise)}" />\n\n`;
+        menu_string_java +=
+            `case "${handleCase(item, capitalise)}":
+                    id = R.id.${handleCase(item, capitalise)};
+                    break;\n\n`;
+        i++;
+    });
+
+
+    //Result
+    $("#generated-menu").val(menu_string_xml);
+    $("#generated-menu-java").val(menu_string_java);
+});  
